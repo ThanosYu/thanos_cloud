16adcca0e33c653ade7ca8143894c52e6b88c333
Z003ZDBC <shiqiang.yu@siemens.com>
2018-09-27 15:22:36 +0800

    first

---
+++ client/src/main/java/com/thanos/ClientApplication.java
@@ -0,0 +1,29 @@
+package com.thanos;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ClientApplication {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "Hi " + name + ",i am from port:" + port;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
